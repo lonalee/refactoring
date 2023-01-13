1c0ba04258ee23f77819d785965ca94bba5a51c0
lonalee2018 <lonalee2018@afidev.com>
2023-01-13 15:29:17 +0900

    Refactoring ~
    1. play 변수 제거
    2. 크레딧 누적 계산하는 함수 생성
    3. 변수에 메소드를 할당하지 않고 함수로 생성해서 사용

--- app.js
+++ app.js
@@ -1,55 +1,92 @@
-function amountFor(perf, play) {
-  let thisAmount = 0;
-  switch (play.type) {
-    case "tragedy":
-      thisAmount = 40000;
-      if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30);
-      }
-      console.log("tragedy", thisAmount);
-      break;
-    case "comedy":
-      thisAmount = 30000;
-      if (perf.audience > 20) {
-        thisAmount += 10000 + 500 * (perf.audience - 30);
-      }
-
-      thisAmount += 300 * perf.audience;
-      console.log("comedy", thisAmount);
-      break;
-
-    default:
-      break;
-  }
-  return thisAmount
-
-}
+const invoices = require('./JSON/invoices.json');
+const plays = require('./JSON/plays.json');
 
 function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format;
+
+  // 메소드를 변수에 할당해서 사용했다. 이것을 함수로 변형한다.
+  function format(aNumber) {
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2,
+    }).format(aNumber / 100);
+  }
+
+  // const format = new Intl.NumberFormat('en-US', {
+  //   style: 'currency',
+  //   currency: 'USD',
+  //   minimumFractionDigits: 2,
+  // }).format;
+
+  /**
+   * @abstract  지역변수 play에 할당하던 것을 함수가 필요한 곳에 바로바로 할당한다.
+   * @description plays json으로부터 공연의 제목/장르를 리턴하는 함수
+   * @param {*} aPerformance
+   * @returns
+   */
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+
+  /**
+   * @description 기존의 switch 구문을 별도의 함수로 추출.
+   * 1. 분기에 따라 thisAmount에 재할당하던 것을
+   * 함수에서 리턴하도록 변경
+   * 2.
+   */
+  function amountFor(perf) {
+    let thisAmount = 0;
+    switch (playFor(perf).type) {
+      case 'tragedy':
+        thisAmount = 40000;
+        if (perf.audience > 30) {
+          thisAmount += 1000 * (perf.audience - 30);
+        }
+        // console.log('tragedy', thisAmount);
+        break;
+      case 'comedy':
+        thisAmount = 30000;
+        if (perf.audience > 20) {
+          thisAmount += 10000 + 500 * (perf.audience - 30);
+        }
+
+        thisAmount += 300 * perf.audience;
+        // console.log('comedy', thisAmount);
+        break;
+
+      default:
+        break;
+    }
+    return thisAmount;
+  }
+
+  function volumeCreditsFor(perf) {
+    let result = 0;
+    result += Math.max(perf.audience - 30, 0);
+
+    if ('comedy' === playFor(perf).type)
+      result += Math.floor(perf.audience / 5);
+    return result;
+  }
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = amountFor(perf, play)
-    volumeCredits += Math.max(perf.audience - 30, 0);
+    /**
+     * plays에는 공연정보
+     * perf는 각 공연 입장 관객 수+공연의 ID
+     */
 
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
-
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${
+    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
       perf.audience
     }석)\n`;
-    totalAmount += thisAmount;
+    totalAmount += amountFor(perf);
+    volumeCredits += volumeCreditsFor(perf);
   }
-  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `총액: ${format(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}\n`;
-  console.log(result);
+  console.log('*** 최종값 ***', result);
   return result;
 }
 
--- app.test.js
+++ app.test.js
@@ -2,7 +2,6 @@ const invoices = require("./JSON/invoices.json");
 const plays = require("./JSON/plays.json");
 const { statement } = require("./app");
 
-test("first", () => {
-  console.log(statement);
+test('first', () => {
   statement(invoices[0], plays);
 });
