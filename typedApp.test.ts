58e630aa37af876e3963766eb179ccaade45a963
lonalee2018 <lonalee2018@afidev.com>
2023-01-17 17:39:33 +0900

    ts-02

--- typedApp.test.ts
+++ typedApp.test.ts
@@ -4,6 +4,8 @@ import { statement } from './typedApp';
 describe('', () => {
   test('ts test 1', () => {
     const res = statement();
-    expect(res).toEqual(1);
+    const expected =
+      '청구 내역 (고객명: BigCo)\nHamlet : $650.00 (55석)\nAs you like it : $580.00 (35석)\nOthello : $500.00 (40석)\n적립포인트 : 47\n총액 : $1,730.00';
+    expect(expected).toEqual(res);
   });
 });
--- typedApp.ts
+++ typedApp.ts
@@ -1,3 +1,68 @@
+import invoice from './JSON/invoice.json';
+import plays from './JSON/plays.json';
+
+interface Performance {
+  playID: string;
+  audience: number;
+}
+
+interface Play {
+  any: {
+    name: string;
+    type: string;
+  };
+}
+
+function amountFor(performances: Performance[], plays: Play) {
+  for (let perf of performances) {
+    const play = plays[perf.playID];
+    let thisAmount = 0;
+
+    switch (play.type) {
+      case 'tragedy':
+        thisAmount = 40000;
+        if (perf.audience > 30) {
+          thisAmount += 1000 * (perf.audience - 30);
+        }
+        break;
+      case 'comedy':
+        thisAmount = 30000;
+        if (perf.audience > 20) {
+          thisAmount += 10000 + 500 * (perf.audience - 20);
+        }
+        thisAmount += 300 * perf.audience;
+        break;
+
+      default:
+        throw new Error(`Unknown Genre ${play.type}`);
+
+        break;
+    }
+
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    if (play.type === 'comedy') volumeCredits += Math.floor(perf.audience / 5);
+
+    result += `${play.name} : ${format(thisAmount / 100)} (${
+      perf.audience
+    }석)\n`;
+    totalAmount += thisAmount;
+  }
+}
+
 export function statement() {
-  return 1;
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+  const format = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  }).format;
+
+  amountFor(invoice.performances, plays);
+
+  result += `적립포인트 : ${volumeCredits}\n`;
+  result += `총액 : ${format(totalAmount / 100)}`;
+
+  return result;
 }
